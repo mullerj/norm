b93d390e6d7fa0b7c3f1986669e5a627200e320a
syermak <syerm001@odu.edu>
2023-02-24 11:30:37 -0500

    Not supported exceptions

--- src/dotnet/src/Mil.Navy.Nrl.Norm/IO/NormOutputStream.cs
+++ src/dotnet/src/Mil.Navy.Nrl.Norm/IO/NormOutputStream.cs
@@ -351,26 +351,26 @@ public class NormOutputStream : Stream
 
     public override int Read(byte[] buffer, int offset, int count)
     {
-        throw new NotImplementedException();
+        throw new NotSupportedException();
     }
 
     public override long Seek(long offset, SeekOrigin origin)
     {
-        throw new NotImplementedException();
+        throw new NotSupportedException();
     }
 
     public override void SetLength(long value)
     {
-        throw new NotImplementedException();
+        throw new NotSupportedException();
     }
 
     public override bool CanRead => false;
 
-    public override bool CanSeek => throw new NotImplementedException();
+    public override bool CanSeek => throw new NotSupportedException();
 
     public override bool CanWrite => IsClosed;
 
-    public override long Length => throw new NotImplementedException();
+    public override long Length => throw new NotSupportedException();
 
-    public override long Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public override long Position { get => throw new NotSupportedException(); set => throw new NotSupportedException(); }
 }
