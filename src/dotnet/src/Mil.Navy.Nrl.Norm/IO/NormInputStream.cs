51128bb7343b938c2ffc22172695392f79658f98
Sylvester Freeman III <Slyfreeman3@gmail.com>
2023-02-24 18:43:24 +0000

    adding message to expections

--- src/dotnet/src/Mil.Navy.Nrl.Norm/IO/NormInputStream.cs
+++ src/dotnet/src/Mil.Navy.Nrl.Norm/IO/NormInputStream.cs
@@ -80,7 +80,7 @@ public class NormInputStream : Stream
     {
         if(tos == null)
         {
-            throw new IOException("");
+            throw new IOException("TOS was null.");
         }
         _normSession.SetTOS(tos);
     }
@@ -173,7 +173,6 @@ public class NormInputStream : Stream
             _closed = true;
         }   
     }
-
 
     public void Dispose()
     {
