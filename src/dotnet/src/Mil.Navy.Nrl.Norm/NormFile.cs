7473c19af7833f90df7bab1616ac0ab8da178092
Sylvester Freeman III <Slyfreeman3@gmail.com>
2023-02-28 15:03:43 +0000

    clean up normfile docs

--- src/dotnet/src/Mil.Navy.Nrl.Norm/NormFile.cs
+++ src/dotnet/src/Mil.Navy.Nrl.Norm/NormFile.cs
@@ -2,6 +2,11 @@
 {
     public class NormFile : NormObject
     {
+        /// <summary>
+        /// Constructor of NormFile
+        /// </summary>
+        /// <param name="handle">type is used to reference state kept for data transport objects being actively transmitted or received.</param>
+        /// <returns>Returns instances of NormFile.</returns>
         internal NormFile(long handle) : base(handle)
         {
 
@@ -11,6 +16,7 @@
         /// This function copies the name, as a NULL-terminated string, of the file object specified by the objectHandle
         /// parameter into the nameBuffer of length bufferLen bytes provided by the application.
         /// </summary>
+        /// <exception cref="IOException">Thrown when failed to get file name.</exception>
         public string Name
         {
             get
@@ -29,6 +35,7 @@
         /// This function renames the file used to store content for the NORM_OBJECT_FILE transport object specified by the objectHandle parameter.This allows receiver applications to rename (or move) received files as needed.NORM
         /// uses temporary file names for received files until the application explicitly renames the file.
         /// </summary>
+        /// <exception cref="IOException">Thrown when failed to rename file.</exception>
         public void Rename(string filePath)
         {
             if(!NormFileRename(_handle, filePath))
