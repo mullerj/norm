367092a5ffab6a82675b995d21a87d602eafa0c5
mullerj <jeffrey.muller3.ctr@us.navy.mil>
2024-07-15 18:34:36 +0000

    Moved extensions to test

---
+++ src/dotnet/tests/Mil.Navy.Nrl.Norm.IntegrationTests/ArrayExtensions.cs
@@ -0,0 +1,17 @@
+using System.IO.MemoryMappedFiles;
+using System.Runtime.InteropServices;
+
+namespace Mil.Navy.Nrl.Norm.IntegrationTests
+{
+    public static class ArrayExtensions 
+    {
+        public static SafeBuffer ToSafeBuffer(this byte[] buffer)
+        {
+            using var memoryMappedFile = MemoryMappedFile.CreateNew(null, buffer.Length);
+            var memoryAccessor = memoryMappedFile.CreateViewAccessor(0, buffer.Length, MemoryMappedFileAccess.ReadWrite);
+            memoryAccessor.WriteArray(0, buffer, 0, buffer.Length);
+            return memoryAccessor.SafeMemoryMappedViewHandle;
+        }
+    }
+}
+
