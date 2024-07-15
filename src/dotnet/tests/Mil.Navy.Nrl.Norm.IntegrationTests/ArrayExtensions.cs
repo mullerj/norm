3f5fcd515301e99ca9bbff9c9fa17160f385a5a1
mullerj <jeffrey.muller3.ctr@us.navy.mil>
2024-07-15 18:38:57 +0000

    Added comments

--- src/dotnet/tests/Mil.Navy.Nrl.Norm.IntegrationTests/ArrayExtensions.cs
+++ src/dotnet/tests/Mil.Navy.Nrl.Norm.IntegrationTests/ArrayExtensions.cs
@@ -3,8 +3,16 @@ using System.Runtime.InteropServices;
 
 namespace Mil.Navy.Nrl.Norm.IntegrationTests
 {
+    /// <summary>
+    /// Extension methods for array
+    /// </summary>
     internal static class ArrayExtensions 
     {
+        /// <summary>
+        /// Obtain a safe buffer from a byte array buffer
+        /// </summary>
+        /// <param name="buffer">The byte array buffer</param>
+        /// <returns>The safe buffer based on the byte array</returns>
         internal static SafeBuffer ToSafeBuffer(this byte[] buffer)
         {
             using var memoryMappedFile = MemoryMappedFile.CreateNew(null, buffer.Length);
